b848c2215ccb98e05b5fa14b5bc29e7f42222ddf
Hugo <>
2024-01-21 19:33:23 +0100

    feat: add all tetrominus and rotation/move

--- src/main/scala/logic/Grid.scala
+++ src/main/scala/logic/Grid.scala
@@ -6,8 +6,13 @@ object Grid {
 
   private val grid = Array.ofDim[Int](rows, cols)
 
-  var currentTetrominus: ITetrominus = ITetrominus(Array(Block(0,0), Block(0,1), Block(0,2), Block(0,3)), 0)
-
+  //var currentTetrominus: Tetrominus = ITetrominus(Array(Block(0,0), Block(0,1), Block(0,2), Block(0,3)), 0)
+  //var currentTetrominus: Tetrominus = ZTetrominus(Array(Block(0,0), Block(1,0), Block(1,1), Block(2,1)), 0)
+  //var currentTetrominus: Tetrominus = STetrominus(Array(Block(0,1), Block(1,1), Block(1,0), Block(2,0)), 0)
+  //var currentTetrominus: Tetrominus = OTetrominus(Array(Block(0,0), Block(0,1), Block(1,0), Block(1,1)), 0)
+  //var currentTetrominus: Tetrominus = LTetrominus(Array(Block(0,0), Block(0,1), Block(0,2), Block(1,2)), 0)
+  //var currentTetrominus: Tetrominus = JTetrominus(Array(Block(1,0), Block(1,1), Block(1,2), Block(0,2)), 0)
+  var currentTetrominus: Tetrominus = TTetrominus(Array(Block(0,0), Block(1,0), Block(2,0), Block(1,1)), 0)
 
   for (i <- 0 until rows; j <- 0 until cols) {
     grid(i)(j) = 0
--- src/main/scala/logic/Tetrominus.scala
+++ src/main/scala/logic/Tetrominus.scala
@@ -8,9 +8,38 @@ sealed abstract class Tetrominus {
   def state: Int
   def rotate: Tetrominus
 
-  def moveLeft: Tetrominus
-  def moveRight: Tetrominus
-  def moveDown: Tetrominus
+  def isValidMove(blocks: Array[Block]): Boolean = {
+    blocks.forall(b => b.x >= 0 && b.x <= 9 && b.y >= 0 && b.y <= 19)
+  }
+
+  def moveLeft: Tetrominus = {
+    val newBlocks = blocks.map(b => b.copy(x = b.x - 1))
+    if (isValidMove(newBlocks)) {
+      this.copy(blocks = newBlocks, state)
+    } else {
+      this
+    }
+  }
+
+  def moveRight: Tetrominus = {
+    val newBlocks = blocks.map(b => b.copy(x = b.x + 1))
+    if (isValidMove(newBlocks)) {
+      this.copy(blocks = newBlocks, state)
+    } else {
+      this
+    }
+  }
+
+  def moveDown: Tetrominus = {
+    val newBlocks = blocks.map(b => b.copy(y = b.y + 1))
+    if (isValidMove(newBlocks)) {
+      this.copy(blocks = newBlocks, state)
+    } else {
+      this
+    }
+  }
+
+  def copy(blocks: Array[Block], state: Int): Tetrominus
 }
 
 case class ITetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
@@ -19,66 +48,88 @@ case class ITetrominus(override val blocks: Array[Block], override val state: In
     val center = blocks(1)
 
     val newBlocks = state match {
-      case 0 =>
-        Array(
-          Block(center.x - 1, center.y),
-          Block(center.x, center.y),
-          Block(center.x + 1, center.y),
-          Block(center.x + 2, center.y)
-        )
-      case 1 =>
-        Array(
-          Block(center.x, center.y - 1),
-          Block(center.x, center.y),
-          Block(center.x, center.y + 1),
-          Block(center.x, center.y + 2)
-        )
-      case 2 =>
-        Array(
-          Block(center.x + 1, center.y),
-          Block(center.x, center.y),
-          Block(center.x - 1, center.y),
-          Block(center.x - 2, center.y)
-        )
-      case 3 =>
-        Array(
-          Block(center.x, center.y + 1),
-          Block(center.x, center.y),
-          Block(center.x, center.y - 1),
-          Block(center.x, center.y - 2)
-        )
+      case 0 => Array(Block(center.x - 1, center.y), Block(center.x, center.y), Block(center.x + 1, center.y), Block(center.x + 2, center.y))
+      case 1 => Array(Block(center.x, center.y - 1), Block(center.x, center.y), Block(center.x, center.y + 1), Block(center.x, center.y + 2))
     }
 
-    if (newBlocks.forall(b => b.x >= 0 && b.x <= 9 && b.y >= 0 && b.y <= 19)) {
-      val newState = (state + 1) % 4
-      ITetrominus(newBlocks, newState)
-    } else {
-      this
-    }
+    if (isValidMove(newBlocks)) ITetrominus(newBlocks, (state + 1) % 2) else this
   }
 
-  def moveLeft: ITetrominus = {
-    val newTetrominus: ITetrominus = this.copy(blocks = blocks.map(b => b.copy(x = b.x - 1)))
-    if (newTetrominus.blocks.forall(b => b.x >= 0)) {
-      newTetrominus
-    } else {
-      this
-    }
+  override def copy(blocks: Array[Block], state: Int): Tetrominus = {
+    ITetrominus(blocks, state)
   }
-  def moveRight: ITetrominus = {
-    val newTetrominus: ITetrominus = this.copy(blocks = blocks.map(b => b.copy(x = b.x + 1)))
-    if (newTetrominus.blocks.forall(b => b.x <= 9)) {
-      newTetrominus
-    } else {
-      this
+}
+case class LTetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  override def rotate: LTetrominus = {
+    val center = blocks(1)
+    val newBlocks = state match {
+      case 0 => Array(Block(center.x - 1, center.y), center, Block(center.x + 1, center.y), Block(center.x - 1, center.y + 1))
+      case 1 => Array(Block(center.x, center.y - 1), center, Block(center.x, center.y + 1), Block(center.x - 1, center.y - 1))
+      case 2 => Array(Block(center.x - 1, center.y), center, Block(center.x + 1, center.y), Block(center.x + 1, center.y - 1))
+      case 3 => Array(Block(center.x, center.y - 1), center, Block(center.x, center.y + 1), Block(center.x + 1, center.y + 1))
     }
+    if (isValidMove(newBlocks)) LTetrominus(newBlocks, (state + 1) % 4) else this
   }
-  def moveDown: ITetrominus = {
-    val newTetrominus: ITetrominus = this.copy(blocks = blocks.map(b => b.copy(y = b.y + 1)))
-    if (newTetrominus.blocks.forall(b => b.y <= 19)) {
-      newTetrominus
-    } else {
-      this
+  override def copy(blocks: Array[Block], state: Int = this.state): LTetrominus = LTetrominus(blocks, state)
+}
+
+case class JTetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  override def rotate: JTetrominus = {
+    val center = blocks(1)
+    val newBlocks = state match {
+      case 0 => Array(Block(center.x - 1, center.y), center, Block(center.x + 1, center.y), Block(center.x - 1, center.y - 1))
+      case 1 => Array(Block(center.x, center.y - 1), center, Block(center.x, center.y + 1), Block(center.x + 1, center.y - 1))
+      case 2 => Array(Block(center.x - 1, center.y), center, Block(center.x + 1, center.y), Block(center.x + 1, center.y + 1))
+      case 3 => Array(Block(center.x, center.y - 1), center, Block(center.x, center.y + 1), Block(center.x - 1, center.y + 1))
     }
+    if (isValidMove(newBlocks)) JTetrominus(newBlocks, (state + 1) % 4) else this
   }
+  override def copy(blocks: Array[Block], state: Int = this.state): JTetrominus = JTetrominus(blocks, state)
+}
+
+case class OTetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  def rotate: OTetrominus = {
+    this
+  }
+
+  override def copy(blocks: Array[Block], state: Int): Tetrominus = {
+    OTetrominus(blocks, state)
+  }
+}
+
+case class STetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  override def rotate: STetrominus = {
+    val center = blocks(1)
+    val newBlocks = state match {
+      case 0 => Array(Block(center.x - 1, center.y - 1), center, Block(center.x - 1, center.y), Block(center.x, center.y + 1))
+      case 1 => Array(Block(center.x + 1, center.y), center, Block(center.x, center.y + 1), Block(center.x - 1, center.y + 1))
+    }
+    if (isValidMove(newBlocks)) STetrominus(newBlocks, (state + 1) % 2) else this
+  }
+  override def copy(blocks: Array[Block], state: Int = this.state): STetrominus = STetrominus(blocks, state)
+}
+case class TTetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  override def rotate: TTetrominus = {
+    val center = blocks(1)
+    val newBlocks = state match {
+      case 0 => Array(Block(center.x, center.y - 1), center, Block(center.x, center.y + 1), Block(center.x - 1, center.y))
+      case 1 => Array(Block(center.x - 1, center.y), center, Block(center.x + 1, center.y), Block(center.x, center.y - 1))
+      case 2 => Array(Block(center.x, center.y + 1), center, Block(center.x, center.y - 1), Block(center.x + 1, center.y))
+      case 3 => Array(Block(center.x + 1, center.y), center, Block(center.x - 1, center.y), Block(center.x, center.y + 1))
+    }
+    if (isValidMove(newBlocks)) TTetrominus(newBlocks, (state + 1) % 4) else this
+  }
+  override def copy(blocks: Array[Block], state: Int = this.state): TTetrominus = TTetrominus(blocks, state)
+}
+
+case class ZTetrominus(override val blocks: Array[Block], override val state: Int) extends Tetrominus {
+  override def rotate: ZTetrominus = {
+    val center = blocks(1)
+    val newBlocks = state match {
+      case 0 => Array(Block(center.x, center.y + 1), center, Block(center.x + 1, center.y), Block(center.x + 1, center.y - 1))
+      case 1 => Array(Block(center.x - 1, center.y), center, Block(center.x, center.y + 1), Block(center.x + 1, center.y + 1))
+    }
+    if (isValidMove(newBlocks)) ZTetrominus(newBlocks, (state + 1) % 2) else this
+  }
+  override def copy(blocks: Array[Block], state: Int = this.state): ZTetrominus = ZTetrominus(blocks, state)
 }
