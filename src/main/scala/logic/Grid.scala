18f9d1d29e48ce1b228ba31229ed198df6ad7b84
Hugo <>
2024-01-28 20:39:03 +0100

    feat: add random generation of tetrominus

--- src/main/scala/logic/Grid.scala
+++ src/main/scala/logic/Grid.scala
@@ -1,20 +1,38 @@
 package logic
+import scala.util.Random
 
 object Grid {
   val cols = 10
   val rows = 20
 
   private val grid = Array.ofDim[Int](rows, cols)
+  private val random = new Random()
 
-  //var currentTetrominus: Tetrominus = ITetrominus(Array(Block(0,0), Block(0,1), Block(0,2), Block(0,3)), 0)
-  //var currentTetrominus: Tetrominus = ZTetrominus(Array(Block(0,0), Block(1,0), Block(1,1), Block(2,1)), 0)
-  //var currentTetrominus: Tetrominus = STetrominus(Array(Block(0,1), Block(1,1), Block(1,0), Block(2,0)), 0)
-  //var currentTetrominus: Tetrominus = OTetrominus(Array(Block(0,0), Block(0,1), Block(1,0), Block(1,1)), 0)
-  //var currentTetrominus: Tetrominus = LTetrominus(Array(Block(0,0), Block(0,1), Block(0,2), Block(1,2)), 0)
-  //var currentTetrominus: Tetrominus = JTetrominus(Array(Block(1,0), Block(1,1), Block(1,2), Block(0,2)), 0)
-  var currentTetrominus: Tetrominus = TTetrominus(Array(Block(0,0), Block(1,0), Block(2,0), Block(1,1)), 0)
+  private val tetrominusGenerators = Array(
+    () => ITetrominus(Array(Block(4,0), Block(4,1), Block(4,2), Block(4,3)), 0),
+    () => JTetrominus(Array(Block(5,0), Block(5,1), Block(5,2), Block(4,2)), 0),
+    () => LTetrominus(Array(Block(4,0), Block(4,1), Block(4,2), Block(5,2)), 0),
+    () => OTetrominus(Array(Block(4,0), Block(4,1), Block(5,0), Block(5,1)), 0),
+    () => STetrominus(Array(Block(4,1), Block(5,1), Block(5,0), Block(6,0)), 0),
+    () => TTetrominus(Array(Block(4,0), Block(5,0), Block(6,0), Block(5,1)), 0),
+    () => ZTetrominus(Array(Block(4,0), Block(5,0), Block(5,1), Block(6,1)), 0)
+  )
+
+  var currentTetrominus: Tetrominus = nextTetrominus
+
+  private def nextTetrominus: Tetrominus = {
+    val nextIndex = random.nextInt(tetrominusGenerators.length)
+    tetrominusGenerators(nextIndex)()
+  }
+
+  private def startGame(): Unit = {
+    currentTetrominus = nextTetrominus
+  }
+
 
   for (i <- 0 until rows; j <- 0 until cols) {
     grid(i)(j) = 0
   }
+
+  startGame()
 }
